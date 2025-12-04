17a6e4f7452e05e5267498886fe6726da6f76720
matheus <matheus.holanda101@gmail.com>
2025-12-04 14:33:00 -0300

    Estrutura dos métodos da classe FileSystemSimulator

--- src/FileSystemSimulator.java
+++ src/FileSystemSimulator.java
@@ -1,2 +1,40 @@
 public class FileSystemSimulator {
+    private Directory root;
+    private Directory currentDirectory;
+    private Journal journal;
+
+    public FileSystemSimulator(Directory root, Journal journal) {
+        this.root = new Directory("/", null);
+        this.currentDirectory = this.root;
+        this.journal = new Journal();
+    }
+
+    public void createDirectory(String directoryName) {
+
+    }
+
+    public void createFile(String fileName) {
+
+    }
+
+    public void deleteDirectory(String directoryName) {
+
+    }
+
+    public void deleteFile(String fileName) {
+
+    }
+
+    public void changeDirectory(String directoryName) {
+
+    }
+
+    public void listCurrentDirectory(String directoryName) {
+
+    }
+
+    public void printJournal() {
+
+    }
+
 }
