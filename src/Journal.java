4ef0f1f1f23e50acae867a677c606b48406ebdcd
matheus <matheus.holanda101@gmail.com>
2025-12-04 14:22:59 -0300

    Classe Journal

--- src/Journal.java
+++ src/Journal.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Journal {
+
+    private List<String> log;
+
+    public Journal(){
+        this.log = new ArrayList<>();
+    }
+
+    public void log(String operation){
+
+    }
+
+    public void printLog(){
+
+    }
 }
