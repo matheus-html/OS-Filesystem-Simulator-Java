635a3a6b731971164743463699f2f9911ccbbdc7
matheus <matheus.holanda101@gmail.com>
2025-12-04 14:13:38 -0300

    Métodos da classe Directory

--- src/Directory.java
+++ src/Directory.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Directory {
+    private String name;
+    private List<File> files;
+    private List<Directory> directories;
+    private Directory parent;
+
+    public Directory(String name, Directory parent) {
+        this.name = name;
+        this.parent = parent;
+        this.files = new ArrayList<>();
+        this.directories = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Directory getParent() {
+        return parent;
+    }
+
+    public void addFile(File file) {
+        this.files.add(file);
+    }
+
+    public void removeFile(String filename) {
+        this.files.remove(new File(filename));
+    }
+
+    public File  getFile(String filename) {
+        return null;
+    }
+
+    public void addDirectory(Directory directory) {
+        this.directories.add(directory);
+    }
+
+    public void removeDirectory(Directory directory) {
+        this.directories.remove(directory);
+    }
+
+    public Directory getDirectory(String directory) {
+        return null;
+    }
+
+    public List<String> listContents() {
+        return null;
+    }
 }
