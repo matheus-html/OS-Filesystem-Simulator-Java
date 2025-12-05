d59f33b61341156bd588461dea1fd3e2dcfca469
matheus <matheus.holanda101@gmail.com>
2025-12-05 18:24:23 -0300

    Métodos completos: FileSystemSimulator

--- src/FileSystemSimulator.java
+++ src/FileSystemSimulator.java
@@ -10,31 +10,61 @@ public class FileSystemSimulator {
     }
 
     public void createDirectory(String directoryName) {
-
+        journal.log("CMD: createDirectory " + directoryName);
+        Directory newDir = new Directory(directoryName, currentDirectory);
+        currentDirectory.addDirectory(newDir);
     }
 
-    public void createFile(String fileName) {
-
+    public void createFile(String fileName, String content) {
+        journal.log("CMD: createFile " + fileName);
+        File newFile = new File(fileName);
+        newFile.setContent(content);
+        currentDirectory.addFile(newFile);
     }
 
     public void deleteDirectory(String directoryName) {
-
+        journal.log("CMD: deleteDirectory " + directoryName);
+        currentDirectory.removeDirectory(directoryName);
     }
 
     public void deleteFile(String fileName) {
-
+        journal.log("CMD: deleteFile " + fileName);
+        currentDirectory.removeFile(fileName);
     }
 
-    public void changeDirectory(String directoryName) {
-
+    public void changeDirectory(String directoryPath) {
+        journal.log("CMD: cd " + directoryPath);
+        if (directoryPath.equals("..")) {
+            if (currentDirectory.getParent() != null) {
+                currentDirectory = currentDirectory.getParent();
+            } else {
+                System.out.println("Já está na raiz.");
+            }
+        } else {
+            Directory target = currentDirectory.getDirectory(directoryPath);
+            if (target != null) {
+                currentDirectory = target;
+            } else {
+                System.out.println("Diretório não encontrado: " + directoryPath);
+            }
+        }
     }
 
-    public void listCurrentDirectory(String directoryName) {
-
+    public void listCurrentDirectory() {
+        journal.log("CMD: ls");
+        System.out.println("Conteúdo de " + currentDirectory.getName() + ":");
+        for (String item : currentDirectory.listContents()) {
+            System.out.println(item);
+        }
     }
 
     public void printJournal() {
+        System.out.println("--- Log de Journaling ---");
+        journal.printLog();
+    }
 
+    public String getCurrentPath() {
+        return currentDirectory.getName();
     }
 
 }
